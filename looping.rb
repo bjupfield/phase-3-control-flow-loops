fd0f65a41e44381992fd5ee541fe26950b1bc8a3
Bennett <bjupfield@gmail.com>
2021-12-13 12:01:09 -0600

    commit tree f02891a6bf2e1751aa458c645e1183521ac643eb

--- looping.rb
+++ looping.rb
@@ -1,5 +1,10 @@
 def happy_new_year
-  # your code here
+  i = 10
+  until i == 0
+    puts i
+    i -= 1
+  end
+  puts "Happy New Year!"
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
@@ -16,9 +21,15 @@ def fizzbuzz(num)
 end
 
 def fizzbuzz_printer
-  # your code here
+  (1..100).each do |i|
+    puts fizzbuzz(i)
+  end
 end
 
 def reverse_string(str)
-  # your code here
+  new_string = ""
+  (0..str.length).each do |char|
+    new_string = str[char, 1] + new_string
+  end
+  new_string
 end
